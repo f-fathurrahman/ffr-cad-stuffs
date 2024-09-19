4542239ec467aafb77172f549f5d48d60608cf91
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-09-19 20:27:42 +0700

    SVG export options

--- learn_cadquery/01_export_svg.py
+++ learn_cadquery/01_export_svg.py
@@ -2,5 +2,19 @@ import cadquery as cq
 from cadquery import exporters
 
 result = cq.Workplane().box(10, 10, 10)
-result.export("IMG_01_box.svg")
+opt = {
+    "width": 300,
+    "height": 300,
+    "marginLeft": 10,
+    "marginTop": 10,
+    "showAxes": False,
+    "projectionDir": (0.5, 0.5, 0.5),
+    "strokeWidth": 0.25,
+    "strokeColor": (255, 0, 0),
+    "hiddenColor": (0, 0, 255),
+    "showHidden": True,
+}
+result.export("IMG_01_box.svg", opt=opt)
+
+
 
